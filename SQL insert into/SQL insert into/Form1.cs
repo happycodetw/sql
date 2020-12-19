21df62dcd161c517c8f7489ee53102408d18c424
crane0221 <75875765+crane0221@users.noreply.github.com>
2020-12-19 16:18:02 +0800

    Add files via upload

---
+++ SQL insert into/SQL insert into/Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace SQL_insert_into
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            using (SqlConnection con = cn.cnn) //連接資料庫
+            {
+                con.Open();//開啓資料庫
+                SqlCommand cmd = new SqlCommand();//宣告執行sql指令的物件   Command=>命令
+                cmd.Connection = con;  //連接sql   Connection=>連接
+                cmd.CommandText =    //打sql指令的地方
+                    $"Select Name From Account";   //Select=>選擇
+                //cmd.ExecuteScalar()    只能一次抓一筆資料
+                SqlDataReader dr = cmd.ExecuteReader();       //一次抓多筆資料
+                while (dr.Read())   //讀取檔案
+                {
+                    if (!dr.Equals(DBNull.Value))   // 直到沒有
+                    {
+                        comboBox1.Items.Add(dr[0].ToString());   // 顯示第一筆資料
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ SQL insert into/SQL insert into/cn.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace SQL_insert_into
+{
+    public static class cn
+    {
+        public static SqlConnection cnn
+        {
+            get
+            {
+                return new SqlConnection("Data Source=.;Initial Catalog=Disneyland_Shanghai_ANO;Integrated Security=True");
+            }
+        }
+    }
+}
